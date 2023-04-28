420f18696d001c705c8692e0e6a78255dbfe3fe9
Juliet Nwagwu Ume-Ezeoke <jnwagwu@stanford.edu>
2023-04-28 15:59:09 -0700

    first

---
+++ units.py
@@ -0,0 +1,35 @@
+from pint import UnitRegistry
+
+
+ureg = UnitRegistry()
+ureg.default_format = '.3f'
+Q_ = ureg.Quantity
+
+# time 
+s = ureg.second
+hour = ureg.hour
+day = ureg.day
+year = ureg.year
+
+# distance 
+m = ureg.meter 
+
+# mass
+g = ureg.gram
+kg = ureg.kilogram
+
+# force
+newton = ureg.newton
+
+# energy
+mj = ureg.megajoule
+j = ureg.joule
+kwh = ureg.kilowatt*ureg.hour
+
+# temperature 
+natural_temp = Q_(15, ureg.degC)
+
+
+# currency 
+ureg.define('USD = currency')
+USD = ureg.USD
